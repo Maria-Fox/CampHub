738b3feb3b11b3dc0c752d8d3aba75f9c4cd4c55
Maria Lazaro <maria.lazaro.fox@gmail.com>
2022-10-03 08:58:49 -0700

    clean up final bits to deploy

--- app.py
+++ app.py
@@ -643,21 +643,3 @@ def suggest_topic(user_id):
     return render_template("user_routes/suggestTopic.html", form = form)
 
 
-##############################################################################
-# Turn off all caching in Flask
-#   (useful for dev; in production)
-#
-# https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
-
-# @app.after_request
-# def add_header(req):
-#     """Add non-caching headers on every request."""
-
-#     req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     req.headers["Pragma"] = "no-cache"
-#     req.headers["Expires"] = "0"
-#     req.headers['Cache-Control'] = 'public, max-age=0'
-#     return req
-    
-
-    
